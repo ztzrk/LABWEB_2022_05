3df74b7932074bf1979e57a9f2099817e8128abc
ztzrk <ralif877@gmail.com>
2022-10-14 16:17:34 +0800

    P5

--- H071211021/Praktikum-4/Nomor3.js
+++ H071211021/Praktikum-4/Nomor3.js
@@ -1,18 +1,30 @@
 var kalimat = prompt("Masukkan Kalimat..");
 
-var arrayHuruf = kalimat.split('').sort();
+var kalimatLower = "";
+for (let i = 0; i < kalimat.length; i++) {
+    var code = parseInt(kalimat.charCodeAt(i));
+    if ((code > 64 && code < 91) || (code > 96 && code < 123)) {
+        kalimatLower += kalimat.charAt(i).toLowerCase();
+    }
+    else {
+        kalimatLower += kalimat.charAt(i);
+    }
+}
+var arrayHuruf = kalimatLower.split('').sort();
 var jumlah = 1;
 var arr = [];
 
 for (let i = 0; i < arrayHuruf.length; i++) {
     const ch = arrayHuruf[i];
     if (ch == arrayHuruf[i+1]) {
+        arr[ch] = jumlah;
         jumlah +=1;
     }
     else {
         arr[ch] = jumlah;
         jumlah = 1;
     }
+    
 }
 
 for (const x in arr) {
